c03a44c7a0273a866cfae58dc044947e8c0151ec
yrx <895987@163.com>
2023-04-01 11:17:05 +0800

    fix(第一章)

---
+++ src/main/java/org/study/chapter01/HelloWord.java
@@ -0,0 +1,7 @@
+package org.study.chapter01;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
---
+++ src/main/resources/note/chapter02.md
@@ -0,0 +1,3 @@
+特别关键字：const、goto是保留关键字
+
+true、false、null不是关键字，是字面量
