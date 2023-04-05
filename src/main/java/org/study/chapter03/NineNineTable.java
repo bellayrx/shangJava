40beccfff119953c36ed4fa78043a46130f1cdd3
yrx <895987@163.com>
2023-04-05 16:26:18 +0800

    feat(嵌套循环的使用)

---
+++ src/main/java/org/study/chapter03/NineNineTable.java
@@ -0,0 +1,16 @@
+package org.study.chapter03;
+
+/**
+ * 乘法表
+ */
+public class NineNineTable {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + "*" + j + "=" + i * j);
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
+}
