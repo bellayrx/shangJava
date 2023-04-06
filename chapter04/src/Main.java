085d7305581ccee28a07700f391176a2ad15b018
yrx <895987@163.com>
2023-04-06 22:37:06 +0800

    feat(一维数组)

---
+++ chapter04/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * ClassName: ${NAME}
+ * Package:
+ * Description:
+ *
+ * @Author wkm
+ * @Create 2023/4/6 21:34
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+}
---
+++ chapter05/src/com/study/one/OneArrayTest.java
@@ -0,0 +1,17 @@
+package com.study.one;
+
+/**
+ * ClassName: OneArrayTest
+ * Package: com.study.one
+ * Description:
+ *
+ * @Author wkm
+ * @Create 2023/4/6 22:35
+ * @Version 1.0
+ */
+public class OneArrayTest {
+    public static void main(String[] args) {
+        int num1 = 10;
+
+    }
+}
