1266cc86fba3340b5215e3d1afd82e5f724ed69d
yrx <895987@163.com>
2023-04-07 14:39:24 +0800

    feat(一维数组)

---
+++ chapter05/src/com/study/exer/ArrayExer1.java
@@ -0,0 +1,41 @@
+package com.study.exer;
+/*
+
+案例：输出英文星期几
+
+用一个数组，保存星期一到星期天的7个英语单词，从键盘输入1-7，显示对应的单词
+{"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"}
+
+拓展：一年12个月的存储
+
+用一个数组，保存12个月的英语单词，从键盘输入1-12，显示对应的单词。
+{"January","February","March","April","May","June","July","August","September","October","November","December"}
+
+ */
+
+import java.util.Scanner;
+
+/**
+ * ClassName: ArrayExer1
+ * Package: com.study.exer
+ * Description:
+ *
+ * @Author wkm
+ * @Create 2023/4/7 14:15
+ * @Version 1.0
+ */
+public class ArrayExer1 {
+    public static void main(String[] args) {
+        String[] weeks = new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入数值：1-7");
+        int day = scanner.nextInt();
+        if (day < 1 || day > 7) {
+            System.out.println("数据有误");
+        } else {
+            System.out.println(weeks[day - 1]);
+        }
+        scanner.close();
+
+    }
+}
---
+++ chapter05/src/com/study/exer/ArrayExer2.java
@@ -0,0 +1,59 @@
+package com.study.exer;
+/*
+
+案例：学生考试等级划分
+
+从键盘读入学生成绩，找出最高分，并输出学生成绩等级。
+    成绩>=最高分-10    等级为’A’
+    成绩>=最高分-20    等级为’B’
+    成绩>=最高分-30    等级为’C’
+    其余              等级为’D’
+
+提示：先读入学生人数，根据人数创建int数组，存放学生成绩。
+ */
+
+import java.util.Scanner;
+
+/**
+ * ClassName: ArrayExer2
+ * Package: com.study.exer
+ * Description:
+ *
+ * @Author wkm
+ * @Create 2023/4/7 14:19
+ * @Version 1.0
+ */
+public class ArrayExer2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("请输入学生人数：");
+        int count = scanner.nextInt();
+        int i = 0;
+        int[] scores = new int[count];
+        int maxScore = scores[0];
+        System.out.println("请输入" + count + "个成绩");
+        while (i < count) {
+            scores[i] = scanner.nextInt();
+            maxScore = Math.max(maxScore, scores[i]);
+            i++;
+        }
+        System.out.println("最高分：" + maxScore);
+        for (int j = 0; j < scores.length; j++) {
+            if (scores[j] >= maxScore - 10) {
+                System.out.println("student " + j + " score is " + scores[j] +
+                        " grade is A");
+            } else if (scores[j] >= maxScore - 20) {
+                System.out.println("student " + j + " score is " + scores[j] +
+                        " grade is B");
+            } else if (scores[j] >= maxScore - 30) {
+                System.out.println("student " + j + " score is " + scores[j] +
+                        " grade is C");
+            } else {
+                System.out.println("student " + j + " score is " + scores[j] +
+                        " grade is D");
+            }
+        }
+
+        scanner.close();
+    }
+}
