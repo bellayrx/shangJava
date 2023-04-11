50705553c394759ff30ceb238ae9597e917e7bf3
yrx <895987@163.com>
2023-04-11 10:32:01 +0800

    feat(方法重载)

---
+++ chapter06/src/com/study/method/more/overload/OverloadTest.java
@@ -0,0 +1,30 @@
+package com.study.method.more.overload;
+
+/**
+ * ClassName: OverloadTest
+ * Package: com.study.method.more.overload
+ * Description:
+ *
+ * @Author wkm
+ * @Create 2023/4/11 10:15
+ * @Version 1.0
+ */
+public class OverloadTest {
+    public void add(int i, int j) {
+    }
+
+    public void add(double i, double j) {
+    }
+
+    public void add(int i, int j, int k) {
+    }
+
+    public void add(String s1, String s2) {
+    }
+
+    public void add(String s1, int i) {
+    }
+
+    public void add(int i, String s1) {
+    }
+}
