a0c5d594ba8b6f652d7c21e1b0dc91192163d625
yrx <895987@163.com>
2023-04-13 17:54:06 +0800

    feat(构造器)

---
+++ chapter06/src/com/study/beanuml/Customer.java
@@ -0,0 +1,35 @@
+package com.study.beanuml;
+
+/**
+ * ClassName: Customer
+ * Package: com.study.beanuml
+ * Description:
+ *
+ * @Author wkm
+ * @Create 2023/4/12 20:52
+ * @Version 1.0
+ */
+public class Customer {
+    private int id;
+    private String name;
+    public Customer() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int i) {
+        id = i;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+}
