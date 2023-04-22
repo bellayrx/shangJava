8ea14448633131293418a54130f1971efa18380c
wkm <895987@163.com>
2023-04-22 13:52:25 +0800

    feat(饿汉 单例)

---
+++ chapter08/src/com/study/singleton/BankTest.java
@@ -0,0 +1,35 @@
+package com.study.singleton;
+
+/**
+ * ClassName: BankTest
+ * Package: com.study.singleton
+ * Description:
+ *
+ * @Author wkm
+ * @Create 2023/4/22 13:47
+ * @Version 1.0
+ */
+public class BankTest {
+    public static void main(String[] args) {
+        Bank bank1 = Bank.getInstance();
+        Bank bank2 = Bank.getInstance();
+
+        System.out.println(bank1 == bank2);
+    }
+}
+
+//饿汉式
+class Bank {
+    //2. 在类的内部创建当前类的实例
+    //4. 此属性也必须声明为static的
+    private static Bank instance = new Bank();
+
+    //1. 类的构造器私有化
+    private Bank() {
+    }
+
+    //3. 使用getXxx()方法获取当前类的实例，必须声明为static的
+    public static Bank getInstance() {
+        return instance;
+    }
+}
